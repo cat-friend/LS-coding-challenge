b58ab2a44a650c59a5f440695c0d2479960edd0d
cat-friend <denisepli@gmail.com>
2022-03-02 09:36:52 -0500

    made post and get route for , get for

---
+++ app/api/loan_routes.py
@@ -0,0 +1,58 @@
+from flask import Blueprint, request
+from app.models import Loan, db
+from app.forms import LoanForm
+
+loan_routes = Blueprint('loans', __name__)
+
+def validation_errors_to_error_messages(validation_errors):
+    """
+    Simple function that turns the WTForms validation errors into a simple list.
+    """
+    errorMessages = []
+    for field in validation_errors:
+        for error in validation_errors[field]:
+            errorMessages.append(f'"{field}: {error}')
+    return errorMessages
+
+
+@loan_routes.route('/', methods=['POST', 'GET'])
+def all_loans():
+    """
+    POST requests - posts new loans with payload from frontend. If successful,
+    the route returns JSON data needed for the frontend. Else, the route
+    returns error messages.
+    GET requests - returns JSON data for all loans.
+    """
+    if request.method == 'GET':
+        loans = Loan.query.all()
+        return {"loans": [loan.to_dict() for loan in loans]}
+    if request.method == 'POST':
+        form = LoanForm()
+        form['csrf_token'].data = request.cookies['csrf_token']
+        if form.validate_on_submit():
+            amount = form.data['amount']
+            interest_rate = form.data['interest_rate']
+            length_months = form.data['length_months']
+            monthly_payment = form.data['monthly_payment']
+            new_loan = Loan(amount=amount, interest_rate=interest_rate, length_months=length_months, monthly_payment=monthly_payment)
+            db.add(new_loan)
+            db.commit()
+            return new_loan.to_dict()
+        else:
+            return {'errors': validation_errors_to_error_messages(form.errors)}, 401
+
+@loan_routes.route('/<int:id>', methods=['GET', 'PUT'])
+def one_loan(id):
+    """
+    PUT requests - updates existing loans with payload from frontend. If successful,
+    the route returns JSON data needed for the frontend. Else, the route
+    returns error messages.
+    GET requests - returns JSON data for specific loan.
+    """
+    if request.method == 'GET':
+        try:
+            loan = Loan.query.filter_by(id=id).all()
+            return loan.to_dict()
+        except:
+            return {'errors': "resource not found"}, 404
+    
