60e4695f9af960f5b1670cb2418f816a778716d1
cat-friend <denisepli@gmail.com>
2022-03-02 21:03:52 -0500

    added footer

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,17 +1,23 @@
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Footer from './components/Footer';
 import Info from './components/Info';
 
 
 function App() {
 
   return (
-    <BrowserRouter>
-      <Switch>
-        <Route path="/" exact={true}>
-          <Info />
-        </Route>
-      </Switch>
-    </BrowserRouter>
+    <>
+      <div>
+        <BrowserRouter>
+          <Switch>
+            <Route path="/" exact={true}>
+              <Info />
+            </Route>
+          </Switch>
+        </BrowserRouter>
+      </div>
+      <Footer />
+    </>
   );
 }
 
---
+++ react-app/src/components/Footer/index.js
@@ -0,0 +1,37 @@
+import "./Footer.css";
+
+function Footer() {
+    return (
+        <div id="footer">
+            <div id="skewed">
+                <div id="bio-links">
+                    <div>Denise Li</div>
+                    <div><a href="mailto:denisepli@gmail.com">Email</a></div>
+                    <div><a href="https://github.com/cat-friend/LS-coding-challenge" target="_blank" rel="noreferrer">GitHub</a></div>
+                    <div><a href="https://www.linkedin.com/in/denise-li-45350320/" target="_blank" id="end" rel="noreferrer">LinkedIn</a></div>
+                </div>
+            </div>
+            <div
+                id="tech">
+                <div>Skills:</div>
+                <div>Python</div>
+                <div>JavaScript</div>
+                <div>React</div>
+                <div>Redux</div>
+                <div>Node</div>
+                <div>Flask</div>
+                <div>WTForms</div>
+                <div>SQLAlchemy</div>
+                <div>Sequelize</div>
+                <div>PostgreSQL</div>
+                <div>Docker</div>
+                <div>Heroku/AWS</div>
+                <div>SDLC</div>
+                <div>Agile methodologies</div>
+                <div>Project management</div>
+                <div>Team leadership</div>
+            </div>
+        </div>
+    )
+}
+export default Footer;
